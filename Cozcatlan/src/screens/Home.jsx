e9964aeb16671f3a8be0b47eb41cb210216bc8a2
Astrid <20240235@ricaldone.edu.sv>
2026-04-07 12:13:51 -0600

    Fix

--- Cozcatlan/src/screens/Home.jsx
+++ Cozcatlan/src/screens/Home.jsx
@@ -1,5 +1,7 @@
 import { Hero } from '../components/home/Hero';
 import { WelcomeSection } from '../components/home/WelcomeSection';
+import "@fontsource/montserrat/400.css"; // Regular
+import "@fontsource/montserrat/700.css"; // Bold
 
 const Home = () => {
   return (
