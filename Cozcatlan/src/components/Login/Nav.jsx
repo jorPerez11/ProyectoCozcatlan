74a55610bb3f9eafa90392120733a36580f75543
jor <20240057@ricaldone.edu.sv>
2026-04-08 11:50:23 -0600

    SignUp terminada, Avance Login

--- Cozcatlan/src/components/Login/Nav.jsx
+++ Cozcatlan/src/components/Login/Nav.jsx
@@ -4,17 +4,12 @@ import React from "react";
 const Nav = () => {
     return(
         
-            <nav className="navbar navbar-expand-lg navbar-light bg-transparent  w-100 top-0 p-1">
+            <nav className="navbar navbar-expand-lg navbar-light bg-transparent position-absolute w-100 top-0 p-4">
             <div className="container-fluid justify-content-end">
                 <div className="d-flex gap-2">
                 {/* Botón Registrarse (Naranja) */}
                 <button className="btn btn-orange px-4 py-2 text-white fw-medium shadow-sm">
                     Registrarse
-                </button>
-                
-                {/* Botón Iniciar Sesión (Blanco con borde verde) */}
-                <button className="btn btnLogin btn-outline-success bg-white px-4 py-2 fw-medium shadow-sm">
-                    Iniciar sesión
                 </button>
                 </div>
             </div>
