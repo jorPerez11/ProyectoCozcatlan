35d833257849593359c6a106f2df5731c54973ec
Amurgas31 <20240235@ricaldone.edu.sv>
2026-04-14 19:37:45 -0600

    Pequeño arreglo del Nav

--- Cozcatlan/src/components/PublicNavbar/Nav.jsx
+++ Cozcatlan/src/components/PublicNavbar/Nav.jsx
@@ -1,50 +1,63 @@
-    import React from 'react';
-    import './NavProducts.css';
-    import logo from '../../assets/logo-cozcatlan.png'; 
-    import shoppingC from '../../assets/shoppingcart.png';
-    import Config from '../../assets/config.png';
+import React from 'react';
+import './NavProducts.css';
+import logo from '../../assets/logo-cozcatlan.png'; 
+import shoppingC from '../../assets/shoppingcart.png';
+import Config from '../../assets/config.png';
 
-    const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg custom-navbar px-4">
-  <div className="container-fluid d-flex align-items-center">
-    
-    {/* LOGO */}
-    <a className="navbar-brand me-auto" href="/">
-      <img src={logo} alt="Cozcatlán" className="navbar-logo" />
-    </a>
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar px-4">
+      <div className="container-fluid d-flex align-items-center">
+        
+        {/* 1. LOGO (Izquierda) */}
+        <a className="navbar-brand me-auto" href="/">
+          <img src={logo} alt="Cozcatlán" className="navbar-logo" />
+        </a>
 
-    {/* BOTÓN MÓVIL (puedes dejarlo pero asegúrate de que el div de abajo no se oculte) */}
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+        {/* 2. ICONOS (Ahora están antes del botón en el código para que en móvil queden a la izquierda del toggle) */}
+        <div className="d-flex align-items-center gap-3 ms-auto icons-container order-lg-last">
+          <a href="/shoppingcart" className="nav-icon">
+            <img src={shoppingC} alt="Carrito" style={{ width: '30px', height: 'auto' }} />
+          </a>
+          <a href="/settings" className="nav-icon">
+            <img src={Config} alt="Configuración" style={{ width: '30px', height: 'auto' }} />
+          </a>
+          
+          {/* 3. BOTÓN HAMBURGUESA (Al final de los iconos en móvil) */}
+          <button 
+            className="navbar-toggler border-0 shadow-none" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
 
-    {/* ENLACES: Quitamos clases que colapsan para asegurar visibilidad */}
-    <div className="navbar-links-center d-none d-lg-flex" id="navbarNav">
-      <ul className="d-flex list-unstyled m-0 gap-4">
-        <li className="nav-item">
-          <a className="nav-link-custom" href="/">Inicio</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link-custom" href="/aboutUs">Sobre Nosotros</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link-custom active" href="/products">Productos</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link-custom" href="/contact">Contacto</a>
-        </li>
-      </ul>
-    </div>
+        {/* 4. ENLACES COLAPSABLES */}
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul className="navbar-nav gap-lg-4 text-center py-3 py-lg-0">
+            <li className="nav-item">
+              <a className="nav-link-custom" href="/">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link-custom" href="/aboutUs">Sobre Nosotros</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link-custom active" href="/products">Productos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link-custom" href="/contact">Contacto</a>
+            </li>
+          </ul>
+        </div>
 
-    {/* ICONOS */}
-    <div className="d-flex align-items-center gap-3 ms-auto icons-container">
-      <a href="/shoppingcart" className="nav-icon"><img src={shoppingC} alt="Carrito" style={{ width: '30px', height: 'auto' }} /></a>
-      <a href="/settings" className="nav-icon"><img src={Config} alt="Configuración" style={{ width: '30px', height: 'auto' }} /></a>
-    </div>
-  </div>
-</nav>
-    );
-    };
+      </div>
+    </nav>
+  );
+};
 
-    export default Navbar;
+export default Navbar;
